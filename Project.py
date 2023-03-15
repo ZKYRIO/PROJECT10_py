6e15b1188fe3f20ba11466beffcbc402c7fe4414
ZKYRIO <hamdanioke1512@gmail.com>
2023-03-15 09:35:26 +0700

    menambahkan file project.py & bikin menu_utama

---
+++ Project.py
@@ -0,0 +1,12 @@
+pilihan_user = int(input("Pilih menu : "))
+if (pilihan_user == 1):
+    # menu_profile()
+    print(".")
+elif (pilihan_user == 2):
+    # menu_soal()
+    print(".")
+elif (pilihan_user == 3):
+    # menu_rangking()
+    print(".")
+else:
+    print("Terima Kasih :)")
